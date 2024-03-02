d9a99a36e713aaa7188d2e9aa39d229ebc7e0e25
samuel andrade <samuelandradesilva3@gmail.com>
2024-03-02 18:29:21 -0300

    aulas c++

---
+++ desafio 7(nível2).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int L, C;
+    int matriz1[L][C];
+    int matriz2[L][C];
+    cout << "Informe a quantidade de linhas que as matrizes terao:" << endl;
+    cin >> L;
+    cout << "Informe a quantidade de colunas que as matrizes terao:" << endl;
+    cin >> C;   
+    return 0;}
